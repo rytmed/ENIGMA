4ca28c61a935ff82ea548ea56e7023841047c45a
Simon Machielsen <masi1407@iepscf-uccle.be>
2022-02-15 00:24:13 +0100

    Suppression des fichiers tests

--- un_autre_test.py
+++
@@ -1,4 +0,0 @@
-print("Bonjour !")
-input("Si tu vois ce programme, c'est que les modifications apportées ont bien fonctionnées !")
-input("Bon courrage pour l'utilisation de git/GitHub !")
-print("Je te renvoie la balle !")
