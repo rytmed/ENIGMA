5b82dc1979c6ca5eb9b1374f373174a4ac8657e3
Simon Machielsen <masi1407@student.iepscf-uccle.be>
2022-02-22 18:40:33 +0100

    aaaaaaaaaaah

---
+++ fonction.py
@@ -0,0 +1,9 @@
+def menu():
+    print(f"Programme de sélection de la machine ENIGMA. Voici les différentes options :\n\n"
+          "1. Lecture message\n"
+          "2. Enregistrer message\n"
+          "3. Cryptage/Décryptage\n"
+          "4. Quitter\n")
+    int(input("Quel choix souhaitez-vous faire ?\n"))
+
+menu()
