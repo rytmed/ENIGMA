87bd6057581101ed1d5d04f6bb7a9694911aac62
dimo nguembou idriss guerin <dimoidrissguerin@yahoo.com>
2022-03-04 13:31:12 +0100

    nettoyage code

--- LectureMessage.py
+++
@@ -1,6 +0,0 @@
-#Fonction de lecture du message entré par l'utilisateur
-def LectureMessage():
-    with open("message.txt", "r", encoding='utf-8') as fichier:
-        return fichier.read()
-
-LectureMessage()
