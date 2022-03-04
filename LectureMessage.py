ca3438fb8009f805f30dd714d9b40d5e57b685a0
ghislain GGI <clay_jnr@MacBook-Pro-de-ghislain.local>
2022-03-04 13:58:57 +0100

    nettoyage branche

--- LectureMessage.py
+++
@@ -1,8 +0,0 @@
-#Fonction de lecture du message entré par l'utilisateur
-def LectureMessage():
-    with open("sources.txt", "r", encoding='utf-8') as fichier:
-        for item in fichier.readlines() :
-            print("/n" + item)
-    return 1
-
-LectureMessage()
