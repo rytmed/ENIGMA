9fd370b1bbb9248d07e203a174a61b4ccf9a54a7
Aceblog4 <95889121+Aceblog4@users.noreply.github.com>
2022-03-04 15:00:09 +0100

    Modifications sur la fonction. Elle convertit le message en enlevant les caractères spéciaux, supprimant les espaces et sauvegarde le résultat dans le fichier 'sources.txt'.

--- fonction_Ecrire_message.py
+++ fonction_Ecrire_message.py
@@ -1,3 +1,7 @@
+
+
+"""
+    Je garde cette version pour avoir une idée si j'ai à nouveau des soucis.
 import unidecode
 def ecrire_de_message(message,nom_de_ficher):
     
@@ -10,3 +14,26 @@ def ecrire_de_message(message,nom_de_ficher):
     return True
 print(ecrire_de_message('Gasien','sources.txt'))
 
+"""
+
+import re
+
+def ecrire_de_message(message, nom_de_ficher):
+    message = message.upper()
+    message = message.replace(" ", "")
+    message = message.replace("É","E")
+    message = message.replace("@","A")
+    message = message.replace("À", "A")
+    message = message.replace("È","E")
+    message = message.replace("€","E")
+    message = message.replace("Ù", "U")
+    message = message.replace("Ê", "E")
+    message = message.replace("$", "S")
+    message = message.replace("Ô","O")
+    new_message = re.sub(r"[^a-zA-Z0-9]","",message)
+    with open("sources.txt", "w") as fichier:
+        fichier.write(new_message)
+    return True
+
+
+print(ecrire_de_message('$alùt@tiôn$ mon c@m@rade', 'sources.txt'))
