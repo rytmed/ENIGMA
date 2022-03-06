56d1b3851c9c80c932500e4c46a769f24baf4ed6
Aceblog4 <95889121+Aceblog4@users.noreply.github.com>
2022-03-06 14:20:25 +0100

    Finition de la fonction de LectureMessage(). Elle a besoin du fichier présent dans la fonction EcrireMessage() pour fonctionner comme prévu.

--- LectureMessage.py
+++ LectureMessage.py
@@ -1,6 +1,33 @@
-#Fonction de lecture du message entré par l'utilisateur
-def LectureMessage():
-    with open("message.txt", "r", encoding='utf-8') as fichier:
-        return fichier.read()
+'''
+3eme option du menu.
+Fonction de lecture du message entré par l'utilisateur.
+'''
+import re  # Bibliothèque des expressions régulières. Va grandement simplifier notre travail.
 
+
+def LectureMessage():
+    global message
+    message = message.upper()  # Passer en lettres majuscules sert à réduire le nombre de cas.
+    message = message.replace(" ", "")
+    message = message.replace("É", "E")
+    message = message.replace("@", "A")
+    message = message.replace("À", "A")
+    message = message.replace("È", "E")
+    message = message.replace("€", "E")
+    message = message.replace("Ù", "U")
+    message = message.replace("Ê", "E")
+    message = message.replace("$", "S")
+    message = message.replace("Ô", "O")
+    message = message.replace("µ", "M")
+    new_message = re.sub(r"[^a-zA-Z0-9]", "", message)
+    with open("sources.txt", "w") as fichier:
+        fichier.write(new_message)
+    return print(f'Le nouveau message, après modifications est donc :', new_message)
+
+
+f = open("sources.txt", "r",
+         encoding='utf-8')  # On ouvre le fichier texte contenant notre message pas encore transformé.
+message = f.read()
+print(f'Le message avant modification est {message}')
+f.close()
 LectureMessage()
